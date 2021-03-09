c5fbd9e783b741b7be3fcd7ac1876ee5d4b42408
Yamil Mateo Rodríguez <yamilmateor@gmail.com>
2021-03-09 20:16:24 +0100

    Add nav bar

---
+++ socialmoney/src/components/App.js
@@ -0,0 +1,13 @@
+import '../App.css';
+import React from "react";
+import "@fontsource/roboto";
+import ButtonAppBar from './ButtonAppBar';
+function App() {
+  return (
+    <div className="App">
+      <ButtonAppBar />
+    </div>
+  );
+}
+
+export default App;
