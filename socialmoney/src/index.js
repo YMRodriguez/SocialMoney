0cc62abbe4906f8d3aa324262afe45e5ab20f35c
Yamil Mateo Rodríguez <yamilmateor@gmail.com>
2021-03-09 17:50:05 +0100

    Create React App

--- socialmoney/src/index.js
+++ socialmoney/src/index.js
@@ -1,14 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import * as Sentry from "@sentry/browser";
-import * as serviceWorker from "./serviceWorker";
-import "typeface-roboto";
-import "./index.css";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-Sentry.init({
-  dsn: process.env.REACT_APP_SENTRY_DSN,
-  release: `${process.env.REACT_APP_NAME}@${process.env.REACT_APP_VERSION}`,
-});
-ReactDOM.render(<App />, document.getElementById("root"));
-serviceWorker.register();
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
