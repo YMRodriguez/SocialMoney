646257ad3ff2fa703fc9540c8fc22d8925f9e20e
jramosf99 <jramosf99@gmail.com>
2021-04-14 13:06:03 +0200

    signup, login, and create post

--- socialmoney/src/components/FormDialog.js
+++ socialmoney/src/components/FormDialog.js
@@ -12,6 +12,7 @@ import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import Checkbox from '@material-ui/core/Checkbox';
+import $ from 'jquery'
 
 export default function FormDialog() {
   const [open, setOpen] = React.useState(false);
@@ -20,11 +21,70 @@ export default function FormDialog() {
     atec: false,
     afun: false,
     comm: false,
+    title: "",
+    content: "",
+    author: "manolo" 
+
   });
+
+   async function makePostRequest(params){
+
+    var url = "http://localhost:8080/SMON-SERVICE/createPost"
+
+    if((state.afun || state.atec || state.opinion) && ((state.content != "") && (state.title != ""))){
+
+      $.ajax({
+        url: url,
+        type: 'POST',
+        data: JSON.stringify(params),
+        async: false, 
+        success: function (msg) {
+            if (msg.code == 200) {
+              handleClose()
+              alert("¡Publicado con éxito!")
+              console.log('Success')
+            }
+            else{
+              alert("Error 404")
+              console.log("Error 404")
+            }
+        }
+      }); 
+
+    }
+    else{
+      if((state.afun == false) || (state.atec==false) || (state.opinion==false)) {
+          alert("Por favor, seleccione al menos una opción ")
+      }
+
+     if(state.content== "") {
+          alert("Por favor, rellene un contenido")
+        }
+     if(state.title== "") {
+          alert("Por favor, rellene un titulo")
+      }
+    } 
+  } 
+ 
+
+    const handleSubmit = () =>{
+    setTimeout(() => {
+      makePostRequest(state) 
+    }, 100);
+  };
+ 
+
 
   const handleChange = (event) => {
     setState({ ...state, [event.target.name]: event.target.checked });
   };
+
+  const handleChangeText = (event) => {
+    setState({ ...state, [event.target.name]: event.target.value });
+  };
+
+
+
 
   const { opinion, atec, afun, comm } = state;
   const error = [opinion, atec, afun].filter((v) => v).length < 1;
@@ -36,6 +96,7 @@ export default function FormDialog() {
   const handleClose = () => {
     setOpen(false);
   };
+
 
   return (
     <div>
@@ -49,6 +110,8 @@ export default function FormDialog() {
             <h2>Título</h2>
           </DialogContentText>
           <TextField
+            onChange={handleChangeText}
+            name="title"
             autoFocus
             margin="dense"
             id="name"
@@ -60,6 +123,8 @@ export default function FormDialog() {
             <h2>Contenido</h2>
           </DialogContentText>
           <TextField
+            onChange={handleChangeText}
+            name="content"
             autoFocus
             margin="dense"
             id="name"
@@ -98,12 +163,15 @@ export default function FormDialog() {
           <Button onClick={handleClose} color="primary">
             Cancel
           </Button>
-          <Button onClick={handleClose} color="primary">
+          <Button onClick={handleSubmit} color="primary">
             Publicar
           </Button>
         </DialogActions>
       </Dialog>
     </div>
   );
+
+
+
 }
 
