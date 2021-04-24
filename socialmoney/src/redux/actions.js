3465a4934fb507fe35d50f1cace18d5ba5715c5f
jramosf99 <jramosf99@gmail.com>
2021-04-24 12:42:53 +0200

    logout working

--- socialmoney/src/redux/actions.js
+++ socialmoney/src/redux/actions.js
@@ -2,6 +2,10 @@ export function userLogged(account){
     return {type: 'USER_LOGGED', payload:{account}}
 }
 
+export function userUnlogged(){
+    return {type: 'USER_UNLOGGED', payload:{}}
+}
+
 export function userVisited(account){
     return {type: 'USER_VISITED', payload:{account}}
 }
