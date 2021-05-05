efe5eec0afa72efe5637b36f634dd87a7771bc94
Yamil Mateo Rodríguez <yamilmateor@gmail.com>
2021-05-06 01:00:14 +0200

    Clean code and delete unused artefacts from the prototype

--- socialmoney/src/redux/ReduxProvider.js
+++ socialmoney/src/redux/ReduxProvider.js
@@ -11,13 +11,10 @@ import LogIn from '../components/LogIn';
 import FullPost from "../components/FullPost.js"
 import SignUp from "../components/SignUp.js"
 import UserProfile from "../components/UserProfile.js"
-import GenericUser from "../components/GenericUser.js"
 import { BrowserRouter, Route } from "react-router-dom";
 import BackgroundVideo from "../components/BackgroundVideo.js";
 import logo from '../logo.png';
 import video from '../FallingCoins.mp4';
-import user from '../user.png';
-import user2 from '../user2.png';
 import ButtonAppBar from '../components/ButtonAppBar';
 import VisitProfile from '../components/VisitProfile';
 
@@ -37,17 +34,15 @@ export default class ReduxProvider extends React.Component {
     render() {
         return (
             <Provider store={this.store}>
-                <div style={{height:'100%'}} >
-                <BrowserRouter>
-                    <Route exact path="/"><BackgroundVideo logo={logo} video={video} /></Route>
-                    <Route path="/feed"><ButtonAppBar logo={logo} user={user} /><App /></Route>
-                    <Route path="/login"><ButtonAppBar logo={logo} user={user} /><LogIn /></Route>
-                    <Route path="/signup"><ButtonAppBar logo={logo} user={user} /><SignUp /></Route>
-                    <Route path="/postId"><ButtonAppBar logo={logo} user={user} /><FullPost user={user} /></Route>
-                    <Route path="/searchprofile"><ButtonAppBar logo={logo} user={user} /><VisitProfile/></Route>
-                    <Route path="/myprofile"><ButtonAppBar logo={logo} user={user} /><UserProfile user={user} /></Route>
-                    <Route path="/author"><ButtonAppBar logo={logo} user={user} /><GenericUser user2={user2} name={"Luis"} /></Route>
-                </BrowserRouter>
+                <div style={{ height: '100%' }} >
+                    <BrowserRouter>
+                        <Route exact path="/"><BackgroundVideo logo={logo} video={video} /></Route>
+                        <Route path="/feed"><ButtonAppBar logo={logo} /><App /></Route>
+                        <Route path="/login"><ButtonAppBar logo={logo} /><LogIn /></Route>
+                        <Route path="/signup"><ButtonAppBar logo={logo} /><SignUp /></Route>
+                        <Route path="/myprofile"><ButtonAppBar logo={logo} /><UserProfile /></Route>
+                        <Route path="/searchprofile"><ButtonAppBar logo={logo} /><VisitProfile /></Route>
+                    </BrowserRouter>
                 </div>
             </Provider>
         );
